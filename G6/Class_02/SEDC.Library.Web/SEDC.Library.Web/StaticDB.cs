4fda10f12aedb10df01934b2fa62f65fa83f8795
Martin <panovski.martin93@gmail.com>
2021-08-05 21:02:24 +0200

    Class 02 demo code added

---
+++ G6/Class_02/SEDC.Library.Web/SEDC.Library.Web/StaticDB.cs
@@ -0,0 +1,32 @@
+﻿using SEDC.Library.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.Library.Web
+{
+    public static class StaticDB
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Harry potter 1"
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Harry potter 2"
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Harry potter 3"
+            }
+        };
+
+        
+    }
+}
