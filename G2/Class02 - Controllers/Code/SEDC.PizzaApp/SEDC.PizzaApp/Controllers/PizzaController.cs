cfac8492164ad674e56ceb3899c2ba704520d2bd
tstojanovska <stojanovskatanja03@gmail.com>
2021-08-07 12:45:42 +0200

    Class 02

---
+++ G2/Class02 - Controllers/Code/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = StaticDb.Pizzas;
+            return View(pizzas); //we send the list to the view
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new EmptyResult();
+            }
+            Pizza pizza = StaticDb.Pizzas.FirstOrDefault(x => x.Id == id);
+            if(pizza == null)
+            {
+                return new EmptyResult();
+            }
+            return View(pizza);
+        }
+        [Route("SeePizzas")] //localhost:[port]/SeePizzas
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
