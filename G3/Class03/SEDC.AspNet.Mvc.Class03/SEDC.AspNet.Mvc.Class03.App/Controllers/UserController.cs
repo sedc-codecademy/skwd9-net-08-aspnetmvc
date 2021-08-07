ca9a29a3ba5187840ad22881c22a754ef0c20407
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-08-07 11:10:54 +0200

    Class03 paer-2

---
+++ G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.AspNet.Mvc.Class03.App.Database;
+using SEDC.AspNet.Mvc.Class03.App.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Mvc.Class03.App.Controllers
+{
+    [Route("profile")]
+    public class UserController : Controller
+    {
+        // /profile/1
+        [HttpGet("{id:int}")]
+        public IActionResult GetProfile(int id)
+        {
+            var user = PizzaDatabase.Users.FirstOrDefault(u => u.Id == id);
+
+            if(user == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var address = PizzaDatabase.Addresses.FirstOrDefault(a => a.UserId == user.Id);
+            var subs = PizzaDatabase.NewsletterSubscription.FirstOrDefault(ns => ns.UserId == id);
+
+            var userDetails = new UserDetailsVM
+            {
+                Id = user.Id,
+                FullName = string.Format("{0} {1}", user.FirstName, user.LastName),
+                Phone = user.Phone,
+                Address = address.Name,
+                IsSubscribed = subs.IsSubscribed ? "Yes" : "No"
+            };
+
+            return View(userDetails);
+        }
+    }
+}
--- G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Models/DataAccessModels/Address.cs
+++ G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Models/DataAccessModels/Address.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SEDC.AspNet.Mvc.Class03.App.Models.DataAccessModels
 {
+    //[Table(nameof(Address))]
     public class Address
     {
+        //[Key]
         public int Id { get; set; }
+        //[MaxLength(50)]
         public string Name { get; set; }
 
+        //[ForeignKey("UserId")]
         public int UserId { get; set; }
     }
 }
