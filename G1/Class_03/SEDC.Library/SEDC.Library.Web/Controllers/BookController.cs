787ddb4d6c73b603d5e99fe09273c438bdf14ca0
Filip <fjanev14@gmail.com>
2021-08-09 21:36:53 +0200

    Class03-G1 added

---
+++ G1/Class_03/SEDC.Library/SEDC.Library.Web/Controllers/BookController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.Library.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.Library.Web.Controllers
+{
+    //[Route("knigi")]
+    public class BookController : Controller
+    {
+        // There is no IActionResult in .Net Framework. 
+        // You must use the particular type of result on each action
+
+        //public ViewResult Index1()
+        //{
+        //    return View();
+        //}
+
+        //[Route("pocetna")]
+        public IActionResult Index()
+        {
+            List<Book> books = StaticDB.Books;
+
+            return View(books);
+        }
+
+
+        public IActionResult BackToPrivacy()
+        {
+            return View("~/Views/Home/Privacy.cshtml");
+        }
+
+
+        public IActionResult OneBook()
+        {
+            Book book = new Book
+            {
+                Id = 1,
+                Title = "Harry Potter and the goblet of fire"
+            };
+
+            return new JsonResult(book);
+            //return new JsonResult(new { Title = "Harry Potter and the goblet of fire"});
+        }
+
+        //[Route("books")]
+        public IActionResult ListOfBooks()
+        {
+            Book book1 = new Book
+            {
+                Id = 1,
+                Title = "Harry Potter and the goblet of fire"
+            };
+
+            Book book2 = new Book
+            {
+                Id = 2,
+                Title = "Lord of the rings"
+            };
+
+            List<Book> books = new List<Book> { book1, book2 };
+
+            return new JsonResult(books);
+            //return new JsonResult(new { Title = "Harry Potter and the goblet of fire"});
+        }
+
+        public IActionResult BackToHome()
+        {
+            // If you pass only one argument that is an Action name
+            // It tries to redirect on that action which is asumed to be in the same controller
+
+            // return RedirectToAction("ListOfBooks");
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Empty()
+        {
+            return new EmptyResult();
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (id > 0)
+            {
+                Book book = StaticDB.Books.SingleOrDefault(x => x.Id == id);
+                return View(book);
+            }
+            else
+            {
+                return new JsonResult(new { Message = "Not found", Code = 404 });
+            }
+        }
+
+        public IActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(Book book)
+        {
+            StaticDB.BookId++;
+
+            book.Id = StaticDB.BookId;
+            StaticDB.Books.Add(book);
+            return RedirectToAction("Index");
+        }
+    }
+}
