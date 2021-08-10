66df3b520e20448d57cf98e3b5378113c9e83a35
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-08-10 19:44:44 +0200

    Class04 part-2

--- G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Controllers/UserController.cs
+++ G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Controllers/UserController.cs
@@ -33,15 +33,64 @@ namespace SEDC.AspNet.Mvc.Class03.App.Controllers
                 FullName = string.Format("{0} {1}", user.FirstName, user.LastName),
                 Phone = user.Phone,
                 Address = address.Name,
-                IsSubscribed = subs.IsSubscribed ? "Yes" : "No"
+                IsSubscribed = subs.IsSubscribed ? "Yes" : "No",
+                UserDetails = GetUserDetails(id)
             };
 
             ViewData["ProfileInfo"] = TempData["ProfileInfo"];
+            ViewData["PageTitle"] = "Profile info";
 
             // dummy data
             ViewBag.DummyData = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-            return View(userDetails);
+            return View("GetProfileCustom", userDetails);
+        }
+
+        private UserDto GetUserDetails(int id)
+        {
+            var user = PizzaDatabase.Users.FirstOrDefault(u => u.Id == id);
+
+            var address = PizzaDatabase.Addresses.FirstOrDefault(a => a.UserId == user.Id);
+            var subs = PizzaDatabase.NewsletterSubscription.FirstOrDefault(ns => ns.UserId == user.Id);
+
+            var orders = PizzaDatabase.Orders.Where(o => o.UserId == user.Id);
+            var pizzaIds = orders.Select(o => o.PizzaId);
+
+            var pizzas = PizzaDatabase.Pizzas.Where(p => pizzaIds.Contains(p.Id));
+
+            var userDto = new UserDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Phone = user.Phone,
+                Address = address.Name,
+                IsSubscribed = subs.IsSubscribed,
+                Orders = new List<OrderDto>()
+            };
+
+            foreach (var order in orders)
+            {
+                var pizza = pizzas.FirstOrDefault(p => p.Id == order.PizzaId);
+
+                var orderDto = new OrderDto
+                {
+                    Id = order.Id,
+                    Delivered = order.Delivered,
+                    Price = pizza.Price,
+                    Pizza = new PizzaDto
+                    {
+                        Name = pizza.Name,
+                        Size = pizza.Size
+                    }
+                };
+
+                userDto.Orders.Add(orderDto);
+            }
+
+            userDto.TotalSpendings = userDto.Orders.Sum(x => x.Price);
+
+            return userDto;
         }
 
         [HttpGet("user-info/{id:int}")]
