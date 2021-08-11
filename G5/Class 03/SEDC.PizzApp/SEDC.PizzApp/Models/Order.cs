afbb1bd29ef2f4d54337666c2a1f7150098ef80a
Radmila <radmila_177@hotmail.com>
2021-08-11 15:09:08 +0200

    Class 03

---
+++ G5/Class 03/SEDC.PizzApp/SEDC.PizzApp/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace SEDC.PizzApp.Models
+{
+   // Domain Model
+   public class Order
+   {
+      public Order()
+      {
+         Id = 1;
+         Pizza = "Kapriciosa";
+         Price = 11;
+      }
+
+      public int Id { get; set; }
+
+      public User User { get; set; }
+
+      public string Pizza { get; set; }
+
+      public double Price { get; set; }
+   }
+}
