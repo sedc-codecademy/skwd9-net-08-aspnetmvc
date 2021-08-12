61b3db7d58494f24beb66782110f77c0127c993d
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2021-08-12 18:51:53 +0200

    Class05 part-1

---
+++ G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Controllers/OrderController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.AspNet.Mvc.Class03.App.Database;
+using SEDC.AspNet.Mvc.Class03.App.Models.DataTransferModels;
+using SEDC.AspNet.Mvc.Class03.App.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class03.App.Controllers
+{
+    [Route("order")]
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData.Add("Title", "Welcome to the Orders page!");
+
+            // pizza name
+            // number of orders
+            // full name
+            // orders
+
+            var orders = PizzaDatabase.Orders;
+            var firstOrder = orders.FirstOrDefault();
+            var user = PizzaDatabase.Users.FirstOrDefault(u => u.Id == firstOrder.UserId);
+
+            var firstPizza = PizzaDatabase.Pizzas.FirstOrDefault(p => p.Id == firstOrder.PizzaId);
+
+            var orderListVM = new OrderListVM
+            {
+                FirstCustomerName = $"{user.FirstName} {user.LastName}",
+                FirstPizza = firstPizza.Name,
+                NumberOfOrders = orders.Count
+            };
+
+            List<OrderDto> listOfOrders = new List<OrderDto>();
+            foreach (var order in orders)
+            {
+                var pizza = PizzaDatabase.Pizzas.FirstOrDefault(p => p.Id == order.PizzaId);
+
+                var orderDto = new OrderDto
+                {
+                    Id = order.Id,
+                    Delivered = order.Delivered,
+                    Price = pizza.Price,
+                    Pizza = new PizzaDto
+                    {
+                        Name = pizza.Name,
+                        Size = pizza.Size
+                    }
+                };
+
+                listOfOrders.Add(orderDto);
+            }
+
+            orderListVM.Orders = listOfOrders;
+
+            return View(orderListVM);
+        }
+    }
+}
---
+++ G3/Class03/SEDC.AspNet.Mvc.Class03/SEDC.AspNet.Mvc.Class03.App/Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.AspNet.Mvc.Class03.App.Models.DataTransferModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Mvc.Class03.App.Models.ViewModels
+{
+    public class OrderListVM
+    {
+        public string FirstPizza { get; set; }
+        public int NumberOfOrders { get; set; }
+        public string FirstCustomerName { get; set; }
+        public List<OrderDto> Orders { get; set; }
+    }
+}
