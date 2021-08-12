b55e5b1b293cf127c9456a97f0c221da24eb7924
LudNaucnik <bajraktarovljube@gmail.com>
2021-08-12 21:03:34 +0200

    .NET MVC G6 Class 04

---
+++ G6/Class_04/SEDC.Library.Web/SEDC.Library.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SEDC.Library.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SEDC.Library.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult WithoutView()
+        {
+            return View();
+        }
+    }
+}
---
+++ G6/Class_04/SEDC.Library.Web/SEDC.Library.Web/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace SEDC.Library.Web.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int PublishingHouseId { get; set; }
+
+        public string GetBookFullName()
+        {
+            return Id + " - " + Title;
+        }
+    }
+}
