10cb0591bf9c76cd00c9a4bab15d3bad61f95c35
Radmila <radmila_177@hotmail.com>
2021-08-13 11:08:45 +0200

    Class 04

---
+++ G5/Class 04/SEDC.PizzApp/SEDC.PizzApp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SEDC.PizzApp.Models;
+using System.Diagnostics;
+
+namespace SEDC.PizzApp.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      public IActionResult Index()
+      {
+         var person = new User
+         {
+            FirstName = "Bojan",
+            LastName = "Zdravkovski",
+            Address = "Ilindenska",
+            Phone = 123123123
+         };
+
+         var order = new Order
+         {
+            Id = 15,
+            User = person,
+            Pizza = "4 sezoni",
+            Price = 300
+         };
+
+         // Using ViewData dictionary
+         ViewData.Add("Title", "Home page ViewData");
+         ViewData.Add("Order", order);
+
+         ViewData["asd"] = "asd";
+
+         // Using ViewBag 
+         ViewBag.Title = "Home page ViewBag";
+         ViewBag.Order = order;
+
+         // Mapping data from domain model to view model
+         var orderDetailsViewModel = new OrderDetailsViewModel
+         { 
+            Address = person.Address,
+            FirstName = person.FirstName,
+            LastName = person.LastName,
+            FullName = person.GetFullName(),
+            Contact = person.Phone,
+            Id = order.Id,
+            Pizza = order.Pizza,
+            Price = order.Price
+         };
+
+         return View(orderDetailsViewModel);
+      }
+
+      public IActionResult Privacy()
+      {
+         return View();
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
+}
---
+++ G5/Class 04/SEDC.PizzApp/SEDC.PizzApp/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzApp.Controllers
+{
+   public class UserController : Controller
+   {
+      [Route("User")]
+      public IActionResult GetUser()
+      {
+         return View("User");
+      }
+
+      public IActionResult GetContact()
+      {
+         return RedirectToAction("Admin", "Admin");
+      }
+
+      [Route("Empty")]
+      public IActionResult GetEmpty()
+      {
+         return new EmptyResult();
+      }
+
+      [Route("Id/{id}")]
+      public IActionResult GetId(int id)
+      {
+         return View("Id");
+      }
+   }
+}
