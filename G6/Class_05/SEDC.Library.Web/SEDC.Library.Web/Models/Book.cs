2edef2d2f9e925079424e73a4842c62b4b7a4010
LudNaucnik <bajraktarovljube@gmail.com>
2021-08-14 12:24:58 +0200

    G6 Class 05

---
+++ G6/Class_05/SEDC.Library.Web/SEDC.Library.Web/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEDC.Library.Web.Models
+{
+    public class Book
+    {
+        [DisplayName("Identifier")]
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public int PublishingHouseId { get; set; }
+
+        public string GetBookFullName()
+        {
+            return Id + " - " + Title;
+        }
+    }
+}
---
+++ G6/Class_05/SEDC.Library.Web/SEDC.Library.Web/StaticDB.cs
@@ -0,0 +1,72 @@
+﻿using SEDC.Library.Web.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SEDC.Library.Web
+{
+    public static class StaticDB
+    {
+        public static List<Book> Books = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Harry potter 1",
+                AuthorId = 1,
+                PublishingHouseId = 1
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Harry potter 2",
+                AuthorId = 1,
+                PublishingHouseId = 1
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Harry potter 3",
+                AuthorId = 1,
+                PublishingHouseId = 1
+            }
+            ,
+            new Book
+            {
+                Id = 4,
+                Title = "Harry potter 4",
+                AuthorId = 1,
+                PublishingHouseId = 1
+            }
+        };
+
+        public static List<Author> Authors = new List<Author>()
+        {
+            new Author
+            {
+                Id = 1,
+                Name = "Joanne K. Rowling",
+                DateOfBirth = Convert.ToDateTime("1965/07/31")
+            },
+            new Author
+            {
+                Id = 2,
+                Name = "Andrzej Sapkowski",
+                DateOfBirth = Convert.ToDateTime("1948/06/21")
+            }
+        };
+
+        public static List<PublishingHouse> PublishingHouses = new List<PublishingHouse>()
+        {
+            new PublishingHouse
+            {
+                Id = 1,
+                Name = "Bloomsbury Publishing"
+            },
+            new PublishingHouse
+            {
+                Id = 2,
+                Name = "SuperNowa"
+            }
+        };
+    }
+}
