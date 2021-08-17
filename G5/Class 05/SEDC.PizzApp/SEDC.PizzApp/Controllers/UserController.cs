c15a744b3203dfb4b6259955bba19db17e5de953
Radmila <radmila_177@hotmail.com>
2021-08-17 22:20:19 +0200

    Class 05

---
+++ G5/Class 05/SEDC.PizzApp/SEDC.PizzApp/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzApp.Models;
+
+namespace SEDC.PizzApp.Controllers
+{
+   public class UserController : Controller
+   {
+      #region EXAMPLES
+
+      //[Route("User")]
+      //public IActionResult GetUser()
+      //{
+      //   return View("User");
+      //}
+
+      //public IActionResult GetContact()
+      //{
+      //   return RedirectToAction("Admin", "Admin");
+      //}
+
+      //[Route("Empty")]
+      //public IActionResult GetEmpty()
+      //{
+      //   return new EmptyResult();
+      //}
+
+      //[Route("Id/{id}")]
+      //public IActionResult GetId(int id)
+      //{
+      //   return View("Id");
+      //}
+
+      //public string GetPositionString(Position position)
+      //{
+      //   return position.ToString();
+      //}
+
+      #endregion
+
+      [HttpGet]
+      public IActionResult Register()
+      {
+         return View();
+      }
+
+      [HttpPost]
+      public IActionResult Register(UserRegisterViewModel userRegister)
+      {
+         // USERREGISTER OBJEKTOT SE KORISTI ZA PONATAMOSNO
+         // ZACUVUVANJE VO BAZA
+
+         var user = new User
+         {
+            FirstName = userRegister.FirstName,
+            LastName = userRegister.LastName,
+            Address = userRegister.Address
+         };
+
+         // Db.Save(user);
+
+         return RedirectToAction("Index", "Home");
+      }
+   }
+}
