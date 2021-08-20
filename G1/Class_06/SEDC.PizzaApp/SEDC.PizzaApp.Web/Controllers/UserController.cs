5440ed0d5d0a1cd43a3eac61a00d7ff9a760c2d7
Martin <panovski.martin93@gmail.com>
2021-08-20 11:39:30 +0200

    Class 06 demo code added

---
+++ G1/Class_06/SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        #region Read operations
+        public IActionResult Index()
+        {
+            List<User> users = StaticDB.Users;
+            return View(users);
+        }
+
+        public IActionResult Details(int id)
+        {
+            User user = StaticDB.Users.SingleOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return View("ResourceNotFound");
+        }
+        #endregion
+
+        #region Create
+        //[HttpGet] - by default it is HttpGet action
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                StaticDB.Users.Add(user);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        #endregion
+
+        #region Update
+        
+        public IActionResult Edit(int id)
+        {
+            User user = StaticDB.Users.SingleOrDefault(x => x.Id == id);
+            if(user != null)
+            {
+                return View(user);
+            }
+            return View("ResourceNotFound");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User userModel)
+        {
+            if(ModelState.IsValid)
+            {
+                User dbUser = StaticDB.Users.SingleOrDefault(x => x.Id == userModel.Id);
+                dbUser.FirstName = userModel.FirstName;
+                dbUser.LastName = userModel.LastName;
+                dbUser.Address = userModel.Address;
+                dbUser.Phone = userModel.Phone;
+
+                //dbUser = userModel;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            User user = StaticDB.Users.SingleOrDefault(x => x.Id == id);
+            return View(user);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ G1/Class_06/SEDC.PizzaApp/SEDC.PizzaApp.Web/Models/Domain/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Models.Domain
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public User User { get; set; }
+    }
+}
