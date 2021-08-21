4ddcacbd4a1fafb1867a74e897a5d91b82fa3eab
tstojanovska <stojanovskatanja03@gmail.com>
2021-08-21 12:41:36 +0200

    Class 07

---
+++ G2/Class07 - Refactor of the app/Code/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Mappers/OrderMapper.cs
@@ -0,0 +1,45 @@
+﻿using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.ViewModels.OrderViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.Mappers
+{
+    public static class OrderMapper
+    {
+        public static OrderListViewModel OrderToOrderListViewModel(Order order)
+        {
+            return new OrderListViewModel
+            {
+                Id = order.Id,
+                Delivered = order.Delivered,
+                UserFullName = $"{order.User.FirstName} {order.User.LastName}",
+                PizzaNames = order.PizzaOrders.Select(x => x.Pizza.Name).ToList()
+            };
+        }
+        //extension method of Order
+        public static OrderDetailsViewModel ToOrderDetailsViewModel(this Order order)
+        {
+            return new OrderDetailsViewModel
+            {
+                Delivered = order.Delivered,
+                PaymentMethod = order.PaymentMethod,
+                Location = order.Location,
+                UserFullName = $"{order.User.FirstName} {order.User.LastName}",
+                PizzaNames = order.PizzaOrders.Select(x => x.Pizza.Name).ToList()
+            };
+        }
+
+        public static Order ToOrder(this OrderViewModel orderViewModel)
+        {
+            return new Order
+            {
+                Delivered = orderViewModel.Delivered,
+                Location = orderViewModel.Location,
+                PaymentMethod = orderViewModel.PaymentMethod,
+                PizzaOrders = new List<PizzaOrder>(),
+                UserId = orderViewModel.UserId
+            };
+        }
+    }
+}
---
+++ G2/Class07 - Refactor of the app/Code/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Services/Implementations/OrderService.cs
@@ -0,0 +1,61 @@
+﻿using SEDC.PizzaApp.DataAccess.Implementations;
+using SEDC.PizzaApp.DataAccess.Interfaces;
+using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.Mappers;
+using SEDC.PizzaApp.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.OrderViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private IRepository<Order> _orderRepository;
+        private IRepository<User> _userRepository;
+        public OrderService(IRepository<Order> orderRepository, IRepository<User> userRepository)
+        {
+            //_orderRepository = new OrderRepository();
+            _orderRepository = orderRepository;
+            _userRepository = userRepository;
+        }
+
+        public void CreateOrder(OrderViewModel orderViewModel)
+        {
+            User userDb = _userRepository.GetById(orderViewModel.UserId);
+            if(userDb == null)
+            {
+                throw new Exception($"User with id {orderViewModel.UserId} was not found!");
+            }
+            Order order = orderViewModel.ToOrder();
+            order.User = userDb;
+
+            int newOrderId = _orderRepository.Insert(order);
+            if(newOrderId <= 0)
+            {
+                throw new Exception("An error occured while saving to db");
+            }
+        }
+
+        public List<OrderListViewModel> GetAllOrders()
+        {
+            //get all orders from db
+            List<Order> dbOrders = _orderRepository.GetAll();
+
+            //map all orders to view models
+            return dbOrders.Select(x => OrderMapper.OrderToOrderListViewModel(x)).ToList();
+        }
+
+        public OrderDetailsViewModel GetOrderDetails(int id)
+        {
+            Order orderDb = _orderRepository.GetById(id);
+            if(orderDb == null)
+            {
+                //log
+                throw new Exception($"The order with id {id} was not found!");
+            }
+            return orderDb.ToOrderDetailsViewModel();
+        }
+    }
+}
