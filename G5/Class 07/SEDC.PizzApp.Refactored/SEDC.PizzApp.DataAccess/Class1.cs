726bf9c7c96457589bf7c3678a30a54a087df18c
Radmila <radmila_177@hotmail.com>
2021-08-21 13:19:41 +0200

    Class 07

---
+++ G5/Class 07/SEDC.PizzApp.Refactored/SEDC.PizzApp.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SEDC.PizzApp.DataAccess
+{
+   public class Class1
+   {
+   }
+}
---
+++ G5/Class 07/SEDC.PizzApp.Refactored/SEDC.PizzApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace SEDC.PizzApp.Models
+{
+   // Domain Model
+   public class User
+   {
+      public string FirstName { get; set; }
+
+      public string LastName { get; set; }
+
+      public string Address { get; set; }
+
+      public long Phone { get; set; }
+
+      public string GetFullName()
+      {
+         return FirstName + " " + LastName;
+      }
+   }
+}
