667645456860de5b68edda239d61202430788fde
LudNaucnik <bajraktarovljube@gmail.com>
2021-08-26 18:36:34 +0200

    G6 Class 09

---
+++ G6/Class_09/SEDC.PizzaApp/SEDC.PizzaApp.DataAccess/Repositories/EntityRepositories/UserEntityRepository.cs
@@ -0,0 +1,60 @@
+﻿using SEDC.PizzaApp.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEDC.PizzaApp.DataAccess.Repositories.EntityRepositories
+{
+    public class UserEntityRepository : IRepository<User>
+    {
+        public List<User> GetAll()
+        {
+            return StaticDb.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return StaticDb.Users.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public int Insert(User entity)
+        {
+            User lastUser = StaticDb.Users.LastOrDefault();
+
+            if (lastUser == null)
+            {
+                entity.Id = 1;
+            }
+            else
+            {
+                entity.Id = lastUser.Id + 1;
+            }
+
+            StaticDb.Users.Add(entity);
+            return entity.Id;
+        }
+
+        public void Update(User entity)
+        {
+            User user = StaticDb.Users.FirstOrDefault(x => x.Id.Equals(entity.Id));
+
+            if (user != null)
+            {
+                user.FirstName = entity.FirstName;
+                user.LastName = entity.LastName;
+                user.Address = entity.Address;
+                user.Username = entity.Username;
+                user.Phone = entity.Phone;
+            }
+        }
+
+        public void DeleteById(int id)
+        {
+            User user = StaticDb.Users.FirstOrDefault(x => x.Id.Equals(id));
+
+            if (user != null)
+            {
+                StaticDb.Users.Remove(user);
+            }
+        }
+    }
+}
---
+++ G6/Class_09/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Domain.Models;
+using SEDC.PizzaApp.Models;
+using SEDC.PizzaApp.Services;
+using System.Collections.Generic;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IUserService _userService;
+        private IPizzaService _pizzaService;
+        private IPizzaOrderService _pizzaOrderService;
+
+        public OrderController(
+            IUserService userService,
+            IPizzaService pizzaService,
+            IPizzaOrderService pizzaOrderService)
+        {
+            _userService = userService;
+            _pizzaService = pizzaService;
+            _pizzaOrderService = pizzaOrderService;
+        }
+
+        public IActionResult Index()
+        {
+            List<Order> orders = _pizzaOrderService.GetAllOrders();
+
+            List<OrderViewModel> viewOrders = new List<OrderViewModel>();
+
+            foreach (Order order in orders)
+            {
+                OrderViewModel orderViewModel = new OrderViewModel();
+
+                orderViewModel.Username = order.User.Username;
+
+                foreach (PizzaOrder pizzaOrder in order.PizzaOrders)
+                {
+                    PizzaViewModel pizzaViewModel = new PizzaViewModel
+                    {
+                        Id = pizzaOrder.Id,
+                        Name = pizzaOrder.Pizza.Name,
+                        Size = pizzaOrder.Pizza.Size
+                    };
+
+                    orderViewModel.Pizzas.Add(pizzaViewModel);
+                }
+
+                viewOrders.Add(orderViewModel);
+            }
+
+            return View(viewOrders);
+        }
+
+        public IActionResult Order(int? pizzas)
+        {
+            OrderViewModel viewModel = new OrderViewModel();
+
+            for (int i = 0; i < pizzas.GetValueOrDefault(); i++)
+            {
+                viewModel.Pizzas.Add(new PizzaViewModel());
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Order([FromForm] OrderViewModel viewModel)
+        {
+            User user = _userService.GetUserByUsername(viewModel.Username);
+            List<Pizza> pizzaList = new List<Pizza>();
+
+            foreach (PizzaViewModel item in viewModel.Pizzas)
+            {
+                Pizza pizza = _pizzaService.GetPizzaByNameAndSize(item.Name, item.Size);
+                pizzaList.Add(pizza);
+            }
+
+            _pizzaOrderService.MakeNewOrder(user, pizzaList);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
