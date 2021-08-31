4b961f6cdd51a4354b21a7e721c59ea2b8743d7b
LudNaucnik <bajraktarovljube@gmail.com>
2021-08-31 18:26:55 +0200

    G6 Class10 updated code

---
+++ G6/Class_10/SEDC.PizzaApp/SEDC.PizzaApp.Services/IPizzaService.cs
@@ -0,0 +1,17 @@
+﻿using SEDC.PizzaApp.Domain.Enums;
+using SEDC.PizzaApp.Domain.Models;
+using System.Collections.Generic;
+
+namespace SEDC.PizzaApp.Services
+{
+    public interface IPizzaService
+    {
+        public List<Pizza> GetAllPizzas();
+
+        public Pizza GetPizzaById(int id);
+
+        public Pizza GetPizzaByNameAndSize(string name, PizzaSize pizzaSize);
+
+        public List<Pizza> GetPizzasForMenu();
+    }
+}
