beb10fca08484e26a8d312c69a237719ff7d4531
Radmila <radmila_177@hotmail.com>
2021-08-31 18:17:27 +0200

    Class 09

---
+++ G5/Class 09/SEDC.PizzApp.Refactored/SEDC.PizzApp/Models/OrderItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SEDC.PizzApp.Refactored.Models
+{
+   public class OrderItemViewModel
+   {
+      public int Id { get; set; }
+
+      public string FirstName { get; set; }
+
+      public string LastName { get; set; }
+
+      public double Price { get; set; }
+
+      public List<PizzaViewModel> Pizzas { get; set; } = new List<PizzaViewModel>();
+   }
+}
